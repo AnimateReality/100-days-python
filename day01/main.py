3264bf0baa1b72ff89ebbfd2f7fc092d8ce3900f
AnimateReality <ronaldchittinappilly@gmail.com>
2022-12-23 16:02:08 +1300

    first commit

---
+++ day01/main.py
@@ -0,0 +1,6 @@
+
+print("Welcome to the band generator program")
+city = input("What city did you grow up in?")
+pet = input("What is the name of your pet?")
+print("A name for your band is: " + city + pet )
+
