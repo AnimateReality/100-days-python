d041013b55f4ffc1918307d7468eb70743f0827e
AnimateReality <ronaldchittinappilly@gmail.com>
2023-01-11 21:16:37 +1300

    while loops

---
+++ day06/main.py
@@ -0,0 +1,15 @@
+def my_function():
+    #Do this 
+    #then do this
+    hello = input("What is your name?\n")
+    print('hello ' + hello)
+my_function()
+
+# While loop
+
+def while_funciton():
+    a = 3
+    while a < 6:
+        a += 1
+    print(a)
+while_funciton()
